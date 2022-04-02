5a5bf3f625624599582b4ce12976210b404fdd3f
PuddingCoke <zhangxianzhe1231@outlook.com>
2022-04-02 15:57:13 +0800

    群居行为演示增加了躲避鼠标的行为

--- src/MyGame.hpp
+++ src/MyGame.hpp
@@ -31,7 +31,7 @@ public:
 		spriteRenderer(SpriteRenderer::create()),
 		shapeRenderer(ShapeRenderer::create())
 	{
-		gsm->push(new FluidSimulationScene(gsm));
+		gsm->push(new SteeringBehaviorScene(gsm));
 		Graphics::setRecordConfig({ 6000ULL,60 });
 	}
 
--- src/SteeringBehavior/SteeringBehaviorScene.cpp
+++ src/SteeringBehavior/SteeringBehaviorScene.cpp
@@ -7,9 +7,14 @@ void SteeringBehaviorScene::handleinput()
 
 void SteeringBehaviorScene::update(const float& dt)
 {
+    const glm::vec3& mousePos = Mouse::getPosition();
     for (size_t i = 0; i < characters.size(); i++)
     {
         characters[i].flock(characters);
+        if (glm::length(glm::vec3(characters[i].pos, 0) - mousePos) < 50.f)
+        {
+            characters[i].apply(characters[i].flee(mousePos) * 10.f);
+        }
         characters[i].update(dt*200.f);
     }
 }
