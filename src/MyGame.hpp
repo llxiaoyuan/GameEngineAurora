aabe77b4693560e9a91797f8f218b375c3c4e1e4
PuddingCoke <zhangxianzhe1231@outlook.com>
2022-04-03 15:31:26 +0800

    更新演示

--- src/MyGame.hpp
+++ src/MyGame.hpp
@@ -31,7 +31,7 @@ public:
 		spriteRenderer(SpriteRenderer::create()),
 		shapeRenderer(ShapeRenderer::create())
 	{
-		gsm->push(new OrthoCameraTestScene(gsm));
+		gsm->push(new FluidSimulationScene(gsm));
 		Graphics::setRecordConfig({ 6000ULL,60 });
 	}
 
